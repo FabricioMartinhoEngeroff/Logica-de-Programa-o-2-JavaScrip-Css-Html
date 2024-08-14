6cddb71b4c4b695e0d22ffbff3833fafbffabfce
Fabricio <fa.engeroff1996@gmail.com>
2024-08-14 06:22:52 -0300

    Primeito commit com projetos css e html completos

---
+++ alugames/js/app.js
@@ -0,0 +1,6 @@
+function alugar(){
+    let game1 = document.getElementById('game-1'); 
+    let game2 = document.getElementById('game-2'); 
+    let game3 = document.getElementById('game-3'); 
+
+}
