27b8d1c88e830272e225b496a299419841649ad6
Fabricio <fa.engeroff1996@gmail.com>
2024-08-14 06:56:28 -0300

    Criado a logicas do botão de alugames

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,6 +1,18 @@
-function alugar(){
-    let game1 = document.getElementById('game-1'); 
-    let game2 = document.getElementById('game-2'); 
-    let game3 = document.getElementById('game-3'); 
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`); 
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button')
+
+
+if (imagem.classList.contains('dashboard__item__img--rented')) {
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.classList.remove('dashboard__item__button--return');
+    botao.textContent = 'Alugar';
+}
+else{
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.textContent = 'Devolver';
+    botao.classList.add('dashboard__item__button--return');
+}
 
 }
