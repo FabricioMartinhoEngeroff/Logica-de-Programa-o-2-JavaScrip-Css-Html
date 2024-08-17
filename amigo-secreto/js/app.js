a9e6b9f39236520fddfc8291040f57e83391c69a
Fabricio <fa.engeroff1996@gmail.com>
2024-08-17 07:31:31 -0300

    Aplicação amigo secreto finalizada.

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -2,6 +2,16 @@ let amigos = [];
 
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
+    if(amigo.value == ''){
+        return;
+    }
+
+    if(amigos.includes(amigo.value)){
+        alert('Nome ja edicionado!!')
+        return;
+    }
+
+
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
     if(lista.textContent == ''){
@@ -14,6 +24,10 @@ function adicionar(){
 }
 
 function sortear(){
+    if(amigos.length < 4){
+        alert('Adicione pelomenos 4 amigos')
+        return;
+    }
   embaralha(amigos);
   let sorteio = document.getElementById('lista-sorteio');
 
